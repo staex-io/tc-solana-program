fa61f0a903c92f6676e56667397297605d8ef6dc
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2025-10-02 15:40:59 +0400

    Update source code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,65 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![allow(unexpected_cfgs)]
+
+use solana_program::{
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
+};
+
+#[derive(borsh::BorshDeserialize)]
+#[cfg_attr(test, derive(borsh::BorshSerialize))]
+struct Data {
+    hash: String,
+    signature: String,
+}
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let data: Data = match borsh::from_slice(instruction_data) {
+        Ok(data) => data,
+        Err(_) => {
+            return Err(solana_program::program_error::ProgramError::BorshIoError(
+                "failed to decode instruction data".to_string(),
+            ));
+        }
+    };
+    msg!("Hash: {}", data.hash);
+    msg!("Signature: {}", data.signature);
+    Ok(())
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod test {
+    use solana_program_test::*;
+    use solana_sdk::{
+        instruction::Instruction, pubkey::Pubkey, signature::Signer, transaction::Transaction,
+    };
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    use crate::Data;
+
+    #[tokio::test]
+    async fn test_hello_world() {
+        let program_id = Pubkey::new_unique();
+        let mut program_test = ProgramTest::default();
+        program_test.add_program("solana", program_id, None);
+        let (banks_client, payer, recent_blockhash) = program_test.start().await;
+
+        let instruction = Instruction {
+            program_id,
+            accounts: vec![],
+            data: borsh::to_vec(&Data {
+                hash: "test_hash".to_string(),
+                signature: "test_signature".to_string(),
+            })
+            .unwrap(),
+        };
+        let mut transaction = Transaction::new_with_payer(&[instruction], Some(&payer.pubkey()));
+        transaction.sign(&[&payer], recent_blockhash);
+
+        let transaction_result = banks_client.process_transaction(transaction).await;
+        assert!(transaction_result.is_ok());
     }
 }
